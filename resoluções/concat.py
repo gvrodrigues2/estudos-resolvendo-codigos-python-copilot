3137cd5473caf39bb26e59be5c21b2c90849f9ab
Gabriel Vasconcelos <127629717+gvrodrigues2@users.noreply.github.com>
2024-07-01 16:12:20 -0300

    add concat

---
+++ resoluções/concat.py
@@ -0,0 +1,8 @@
+
+dado1 = input("Por favor, insira o primeiro dado: ")
+
+dado2 = input("Por favor, insira o segundo dado: ")
+
+resultado = dado1 + dado2
+
+print("Resultado da concatenação: ", resultado)
